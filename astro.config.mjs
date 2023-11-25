09a85dac3cd7a07f9bcab7ea070931fe85d88005
Agoudjilissam <95285029+Agoudjiliss@users.noreply.github.com>
2023-11-25 14:14:07 +0100

    i
    v2

---
+++ astro.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import vercel from '@astrojs/vercel/serverless';
+import alpinejs from "@astrojs/alpinejs";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), alpinejs()],
+  output: 'server',
+  adapter: vercel({
+    analytics: true,
+    site: 'https://github.com/Agoudjiliss/issam_agoudjil.github.io',
+   
+  }),
+});
